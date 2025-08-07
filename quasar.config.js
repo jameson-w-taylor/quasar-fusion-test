893583068ae99bfacfc53a62206dbe0dcdfee1e3
Jameson Taylor <jameson@builder.io>
2025-08-07 16:08:43 -0400

    add more config

--- quasar.config.js
+++ quasar.config.js
@@ -21,6 +21,49 @@ if (process.env.HMR && process.env.HMR === 'default') {
   };
 }
 let proxy = {};
+if (process.env.MOCK_API && process.env.MOCK_API === 'proxy') {
+  const mockApiPort = process.env.MOCK_API_PORT || 3000;
+  host = 'localhost';
+  const proto = process.env.MOCK_API_PROTO || 'http';
+  const target = `${proto}://${host}:${mockApiPort}`
+  // Proxy API requests to the mock server
+  // This is useful for development with a mock API server
+  proxy = {
+    '/api': {
+      target,
+      changeOrigin: true,
+      secure: false,
+      ws: true,
+      logLevel: 'debug',
+      headers: {
+        'X-Forwarded-Host': host,
+        'X-Forwarded-Proto': proto,
+      },
+    },
+    '/campaigns/api': {
+      target,
+      changeOrigin: true,
+      secure: false,
+      ws: true,
+      logLevel: 'debug',
+      headers: {
+        'X-Forwarded-Host': host,
+        'X-Forwarded-Proto': proto,
+      },
+    },
+    '/campaigns/export-data': {
+      target,
+      changeOrigin: true,
+      secure: false,
+      ws: true,
+      logLevel: 'debug',
+      headers: {
+        'X-Forwarded-Host': host,
+        'X-Forwarded-Proto': proto,
+      },
+    },
+  };
+}
 
 export default defineConfig((/* ctx */) => {
   return {
